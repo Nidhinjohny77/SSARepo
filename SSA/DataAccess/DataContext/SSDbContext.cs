819a2b119d5e9498e8c98e8a7df51c01d7d74907
NidhinJohny77 <nidhinjohny77@gmail.com>
2022-11-19 20:19:12 +0000

    Added migration script for property related tables.

--- SSA/DataAccess/DataContext/SSDbContext.cs
+++ SSA/DataAccess/DataContext/SSDbContext.cs
@@ -48,6 +48,23 @@ namespace DataAccess.DataContext
             modelBuilder.Entity<PropertyImage>().HasKey(pi => pi.UID);
             modelBuilder.Entity<PropertyImage>().HasOne(p=>p.ImageFile).WithOne().HasForeignKey<PropertyImage>(p => p.ImageFileUID);
             modelBuilder.Entity<PropertyImage>().HasOne<Property>().WithMany(p=>p.Images).HasForeignKey(pi=>pi.PropertyUID);
+
+            modelBuilder.Entity<PropertyListing>().ToTable("PropertyListing");
+            modelBuilder.Entity<PropertyListing>().HasKey(pl => pl.UID);
+            modelBuilder.Entity<PropertyListing>().HasOne<Property>().WithMany(p => p.Listings).HasForeignKey(pl => pl.PropertyUID);
+
+            modelBuilder.Entity<PropertyViewing>().ToTable("PropertyViewing");
+            modelBuilder.Entity<PropertyViewing>().HasKey(pv => pv.UID);
+            modelBuilder.Entity<PropertyViewing>().HasOne<PropertyListing>().WithMany(pl => pl.Viewings).HasForeignKey(pv => pv.PropertyListingUID);
+
+            modelBuilder.Entity<PropertyRenting>().ToTable("PropertyRenting");
+            modelBuilder.Entity<PropertyRenting>().HasKey(pr => pr.UID);
+            modelBuilder.Entity<PropertyRenting>().HasOne<PropertyListing>().WithMany(pl => pl.Rentings).HasForeignKey(pr => pr.PropertyListingUID);
+
+            modelBuilder.Entity<PropertyReview>().ToTable("PropertyReview");
+            modelBuilder.Entity<PropertyReview>().HasKey(pr => pr.UID);
+            modelBuilder.Entity<PropertyReview>().HasOne<Property>().WithMany(p => p.Reviews).HasForeignKey(pr => pr.PropertyUID);
+
         }
 
        public DbSet<Student> Students { get; set; }
@@ -58,7 +75,7 @@ namespace DataAccess.DataContext
        public DbSet<ImageFile> ImageFiles { get; set; }
        public DbSet<Property> Properties { get; set; } 
        public DbSet<PropertyImage> PropertyImages { get; set; }   
-        //public DbSet<PropertyListing> PropertyListings { get; set; }
+       public DbSet<PropertyListing> PropertyListings { get; set; }
         // public DbSet<PropertyViewing> PropertyViewings { get; set; }   
         //public DbSet<PropertyRenting> PropertyRentings { get; set; }   
         //public DbSet<PropertyReview> PropertyReviews { get; set; }   
--- SSA/DataAccess/Entities/PropertyViewing.cs
+++ SSA/DataAccess/Entities/PropertyViewing.cs
@@ -11,6 +11,8 @@ namespace DataAccess.Entities
         public DateTime EndDateTime { get; set; }
         public int status { get; set; }
         public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
         public string LastUpdatedBy { get; set; }
         public DateTime LastUpdatedDate { get; set; }
     }
