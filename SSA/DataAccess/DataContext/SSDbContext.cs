5f8c4e4702facb7d4d25db5093fb6b6377962f29
NidhinJohny77 <nidhinjohny77@gmail.com>
2022-11-19 17:56:38 +0000

    Added migration to create User and Roles table.

--- SSA/DataAccess/DataContext/SSDbContext.cs
+++ SSA/DataAccess/DataContext/SSDbContext.cs
@@ -16,20 +16,23 @@ namespace DataAccess.DataContext
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>(entity => entity.HasOne(p=>p.Role).WithOne());
+            modelBuilder.Entity<Role>().HasKey(r=>r.UID);
+            modelBuilder.Entity<User>().HasKey(u => u.UID);
+            modelBuilder.Entity<User>(entity => entity.HasOne(p => p.Role));
+                //.Entity<PropertyImage>(entity=>entity.HasOne(p=>p.ImageFileUID));
         }
 
-        public DbSet<Student> Students { get; set; }
-        public DbSet<University> Universities { get; set; } 
+        //public DbSet<Student> Students { get; set; }
+        //public DbSet<University> Universities { get; set; } 
         public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Landlord> Landlords { get; set; }
-        public DbSet<Property> Properties { get; set; } 
-        public DbSet<PropertyImage> PropertyImages { get; set; }   
-        public DbSet<PropertyListing> PropertyListings { get; set; }
-        public DbSet<PropertyViewing> PropertyViewings { get; set; }   
-        public DbSet<PropertyRenting> PropertyRentings { get; set; }   
-        public DbSet<PropertyReview> PropertyReviews { get; set; }
-        public DbSet<ImageFile> ImageFiles { get; set; }    
+       public DbSet<Role> Roles { get; set; }
+       // public DbSet<Landlord> Landlords { get; set; }
+        //public DbSet<Property> Properties { get; set; } 
+        //public DbSet<PropertyImage> PropertyImages { get; set; }   
+        //public DbSet<PropertyListing> PropertyListings { get; set; }
+       // public DbSet<PropertyViewing> PropertyViewings { get; set; }   
+        //public DbSet<PropertyRenting> PropertyRentings { get; set; }   
+        //public DbSet<PropertyReview> PropertyReviews { get; set; }
+        //public DbSet<ImageFile> ImageFiles { get; set; }    
     }
 }
--- SSA/DataAccess/Entities/PropertyImage.cs
+++ SSA/DataAccess/Entities/PropertyImage.cs
@@ -3,10 +3,14 @@ namespace DataAccess.Entities
 {
     public class PropertyImage
     {
+        public string UID { get; set; }
         public string PropertyUID { get; set; }
+        
         public string ImageFileUID { get; set; }
         public bool IsActive { get; set; }
         public string LastUpdatedBy { get; set; }
         public DateTime LastUpdatedDate { get; set; }
+
+        public ImageFile ImageFile { get; set; }
     }
 }
