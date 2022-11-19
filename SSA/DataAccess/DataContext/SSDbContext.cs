37083afe13532b992d0fd198b10e6187190675ba
NidhinJohny77 <nidhinjohny77@gmail.com>
2022-11-19 16:20:33 +0000

    Added dbsets for all entities.

--- SSA/DataAccess/DataContext/SSDbContext.cs
+++ SSA/DataAccess/DataContext/SSDbContext.cs
@@ -16,11 +16,20 @@ namespace DataAccess.DataContext
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.Entity<User>(entity => entity.HasOne(p=>p.Role).WithOne());
         }
 
         public DbSet<Student> Students { get; set; }
         public DbSet<University> Universities { get; set; } 
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Landlord> Landlords { get; set; }
+        public DbSet<Property> Properties { get; set; } 
+        public DbSet<PropertyImage> PropertyImages { get; set; }   
+        public DbSet<PropertyListing> PropertyListings { get; set; }
+        public DbSet<PropertyViewing> PropertyViewings { get; set; }   
+        public DbSet<PropertyRenting> PropertyRentings { get; set; }   
+        public DbSet<PropertyReview> PropertyReviews { get; set; }
+        public DbSet<ImageFile> ImageFiles { get; set; }    
     }
 }
